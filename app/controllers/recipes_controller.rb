1ed50c2c40606b0c09fd19f5e57a9b476a1c4d78
katebeavis <katebeavis84@gmail.com>
2016-11-23 09:39:19 +0000

    changed ordering of recipes to updated at

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = Recipe.all.order(created_at: :desc)
+    @recipes = Recipe.all.order(updated_at: :desc)
   end
 
   def new
