e7b6f2d282e295daea75a7936f30bdd675ea6271
katebeavis <katebeavis84@gmail.com>
2016-12-21 15:58:24 +0000

    added type-it

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -4,6 +4,23 @@ ready = function() {
       $(this).effect('bounce', {distance: 5, times:1}, 400);
   });
 
+  $('.type-it').typeIt({
+       speed: 80,
+       lifeLike: true
+  })
+  .tiType('&ldquo; Helping you to bake ')
+  .tiPause(1000)
+  .tiSettings({speed: 80})
+  .tiType('good &rdquo;')
+  .tiPause(800)
+  .tiDelete(6)
+  .tiPause(800)
+  .tiType('great &rdquo;')
+  .tiPause(800)
+  .tiDelete(7)
+  .tiPause(1500)
+  .tiType('well &rdquo;')
+
   $(function() {
     $('a[href*="#"]:not([href="#"])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
--- spec/features/recipe_spec.rb
+++ spec/features/recipe_spec.rb
@@ -1,14 +1,38 @@
 require 'rails_helper'
 require './spec/support/features/login_macros.rb'
-include LoginMacros
 
 RSpec.describe 'Recipe page', type: :feature do
+  before(:all) do
+    Capybara.current_driver = :selenium
+  end
+  include LoginMacros
   let(:user) { FactoryGirl.create(:user) }
   let(:new_recipe_page) { Pages::NewRecipe.new }
 
   context 'adding a recipe' do
     before do
       sign_in(user)
+    end
+
+    scenario 'removing ingredient fields' do
+      visit recipes_path
+
+      click_link 'Add recipe'
+
+      new_recipe_page.fill_in_title
+
+      find('.chevron-anchor-recipes').click
+
+      new_recipe_page.fill_in_ingredient(0, 'Sugar', 200, 'g', 1.50, 1, 'kg')
+
+      remove_links = page.all(".remove_fields")
+      remove_links[1].click
+      remove_links[2].click
+      remove_links[3].click
+
+      click_button 'Create recipe'
+      expect(page).to have_content('Mini Bakewells')
+      expect(page).to have_current_path(recipe_path(Recipe.last))
     end
 
     scenario 'adding a recipe' do
@@ -49,7 +73,7 @@ RSpec.describe 'Recipe page', type: :feature do
       expect(page).to have_content('2')
     end
 
-    scenario 'removing ingredient fields' do
+    xscenario 'adding ingredient fields' do
       visit recipes_path
 
       click_link 'Add recipe'
@@ -60,10 +84,15 @@ RSpec.describe 'Recipe page', type: :feature do
 
       new_recipe_page.fill_in_ingredient(0, 'Sugar', 200, 'g', 1.50, 1, 'kg')
 
-      remove_links = page.all(".remove_fields")
-      remove_links[1].click
-      remove_links[2].click
-      remove_links[3].click
+      new_recipe_page.fill_in_ingredient(1, 'Butter', 100, 'g', 2, 250, 'g')
+
+      new_recipe_page.fill_in_ingredient(2, 'Flour', 500, 'g', 2, 1.50, 'kg')
+
+      new_recipe_page.fill_in_ingredient(3, 'Egg', 2, 'unit', 1, 10, 'unit')
+
+      click_link 'Add another ingredient'
+
+      new_recipe_page.fill_in_ingredient(1482253092426, 'Ground almond', 100, 'g', 1.50, 200, 'g')
 
       click_button 'Create recipe'
       expect(page).to have_current_path(recipe_path(Recipe.last))
